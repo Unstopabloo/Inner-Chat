a7fab743fa420dc90964df9c47ed658fe40c3cb1
Unstopabloo <pablooyarceramirez@gmail.com>
2023-12-09 18:53:15 -0300

    Changing model

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ async def ask_openai(question: Question):
   )
 
   response = client.chat.completions.create(
-    model="gpt-4",
+    model="gpt-3.5-turbo-1106",
     messages=[
         {
           "role": "system",
